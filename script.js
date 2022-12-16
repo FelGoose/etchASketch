c86bd85f15edece91db5d1b06b6681622f5350b0
Felicia Aho <felgoose@pop-os.localdomain>
2022-12-16 13:09:02 +0100

    mouseover effect

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-let size = 1000;
+let size = 256;
 for(i=0; i<size; i++){
     const box = document.createElement("div");
     box.classList.add("box");
@@ -11,8 +11,15 @@ for(i=0; i<size; i++){
 
 const boxes = document.querySelectorAll(".box");
 
+addEventListener("mouseover", (e) => {
+    if (e.target.classList[0] !== "box") return;
+    e.target.classList.add("hover");
+});
+
 addEventListener("click", (e) =>{
+    if (e.target.classList[0] !== "box") return;
+
     console.log("click ");
-    console.log(e.target);
+    console.log(e.target.classList);
     e.target.classList.toggle("active");
-})
+});
