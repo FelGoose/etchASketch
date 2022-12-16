5b6cfa0c841d98ede16e5178da280b2021cfacd5
Felicia Aho <felgoose@pop-os.localdomain>
2022-12-16 12:50:16 +0100

    now paints on the grid you click on

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.createElement("div");
-
 container.classList.add("container");
-
 document.body.appendChild(container);
 
 let size = 1000;
-
 for(i=0; i<size; i++){
     const box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
 }
 
-const clicker = addEventListener("click", (e) =>{
-    console.log("click");
+const boxes = document.querySelectorAll(".box");
+
+addEventListener("click", (e) =>{
+    console.log("click ");
+    console.log(e.target);
+    e.target.classList.toggle("active");
 })
