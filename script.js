8d2761240bebd1b8dbc766bd21743a8f9dad050c
Felicia Aho <felgoose@pop-os.localdomain>
2023-04-24 08:35:55 +0200

    the app is finally working!

--- script.js
+++ script.js
@@ -2,19 +2,61 @@ const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-let size = 256;
-for(i=0; i<size; i++){
-    const box = document.createElement("div");
-    box.classList.add("box");
-    container.appendChild(box);
-}
+//default inputSize
+let DefaultBoxesPerSide = 10;
 
-// addEventListener("click", (e) =>{
-//     if (e.target.classList[0] !== "box") return;
-//     e.target.classList.toggle("active");
-// });
+/* let canvasSize = 256 * 4; 
+1024*/
+let canvasSize = 500;
 
-addEventListener("mouseover", (e) => {
+newDrawing(DefaultBoxesPerSide);
+
+
+ // mouseover boxes
+ addEventListener("mouseover", (e) => {
     if (e.target.classList[0] !== "box") return;
     e.target.classList.add("hover");
 });
+
+// size button
+const btnBoxSize = document.querySelector('#boxSize');
+ btnBoxSize.addEventListener("click", (e) =>{
+    changeSize();
+ });
+
+// changeSize
+function changeSize(){
+    boxesPerSide = prompt("How many boxes per side? (Max 100)"); 
+    if(boxesPerSide > 100){
+        return(alert("too many boxes! (max 100)"))
+    }
+    removeDrawing(boxesPerSide);
+    newDrawing(boxesPerSide);
+}
+
+// new drawing
+function newDrawing(boxesPerSide){
+    // box amount
+    for(i=0; i<boxesPerSide * boxesPerSide; i++){
+
+        let boxSize = (canvasSize / boxesPerSide);
+
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px`;
+        container.appendChild(box);
+    }
+}
+
+//remove drawing
+function removeDrawing(){
+    const allBoxes = document.querySelectorAll(".box");
+    
+    for(i=0; i<allBoxes.length ; i++){
+        const box = document.querySelector(".box");
+        box.classList.remove("hover");
+        container.appendChild(box);
+
+        container.removeChild(box);
+    }
+}
